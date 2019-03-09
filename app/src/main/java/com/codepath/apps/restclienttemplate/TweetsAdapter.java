d5b929cf0162312a598254f553a2fb038d74b01c
oleksandrbi <o.bihary@gmail.com>
2019-03-09 16:41:18 -0500

    initial

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,83 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import static com.codepath.apps.restclienttemplate.R.id.ivProfileImage;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder>
+{
+    private Context context;
+    private List<Tweet> tweets;
+
+    // Pass in context & list tweets
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    // for each row inflate layout
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // bind values based on pos. of element
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position)
+    {
+        Tweet tweet = tweets.get(position);
+        holder.tvBody.setText(tweet.body);
+        holder.tvScreenName.setText(tweet.user.screenName);
+        Glide.with(context).load(tweet.user.profileImageUrl).into(holder.ivProfileImage);
+    }
+
+    // define the viewholder
+    @Override
+    public int getItemCount()
+    {
+        return tweets.size();
+    }
+
+    public void clear()
+    {
+        tweets.clear;
+        notifyDataSetChanged();
+    }
+
+    public void addTweets(List<Tweet> tweetList)
+    {
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        public ImageView ivProfileImage;
+        public TextView tvScreenName;
+        public TextView tvBody;
+
+        public ViewHolder(View itemView)
+        {
+            super(itemView);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvBody = itemView.findViewById(R.id.tvBody);
+        }
+    }
+}
